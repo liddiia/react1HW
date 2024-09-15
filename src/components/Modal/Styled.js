8f8da951429c57650ad157e8d08b5bf0b9fe8cf5
liddiia <liddabojko@gmail.com>
2024-09-15 13:14:45 +0300

    summury

--- src/components/Modal/Styled.js
+++ src/components/Modal/Styled.js
@@ -24,12 +24,12 @@ max-width:450px;
 width:100%;
 min-height:450px;
 background-color: white;
-border-radius: 10px;
+border-radius: 8px;
 position: absolute;
 left:50%;
 top:50%;
 transform: translate(-50%, -50%);
-padding: 20px;
+padding: 24px;
 &:hover{
 cursor: auto;
 }
